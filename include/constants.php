ffed8a2146a15d68bba4b36d67643b1957901018
osteel <yannickchenot@gmail.com>
2017-04-26 18:13:42 +0100

    removed config constants

--- include/constants.php
+++ include/constants.php
@@ -4,8 +4,3 @@
 // This code is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
 //
 define("BASE_DIR", dirname(dirname(__FILE__)));
-define("WOWZA_HOST","http://localhost:8087/v2");
-define("WOWZA_SERVER_INSTANCE", "_defaultServer_");
-define("WOWZA_VHOST_INSTANCE", "_defaultVHost_");
-define("WOWZA_USERNAME", "admin");
-define("WOWZA_PASSWORD", "admin");
